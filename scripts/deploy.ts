14bcad69ebaaeded1799aa05e598008fbd476e7a
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-12-18 14:32:39 +0100

    ♻️ Update to `Node.js` Version `20`
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,21 +1,28 @@
 import hre from "hardhat";
 
+// Colour codes for terminal prints
+const RESET = "\x1b[0m";
+const GREEN = "\x1b[32m";
+
 function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function main() {
-  const contract = await hre.ethers.deployContract("Create");
+  const create = await hre.ethers.deployContract("Create");
 
-  await contract.waitForDeployment();
-  const contractAddress = await contract.getAddress();
+  await create.waitForDeployment();
+  const createAddress = await create.getAddress();
 
-  console.log("Create deployed to:", contractAddress);
+  console.log("Create deployed to: " + `${GREEN}${createAddress}${RESET}\n`);
 
+  console.log(
+    "Waiting 30 seconds before beginning the contract verification to allow the block explorer to index the contract...\n",
+  );
   await delay(30000); // Wait for 30 seconds before verifying the contract
 
   await hre.run("verify:verify", {
-    address: contractAddress,
+    address: createAddress,
   });
 }
 
