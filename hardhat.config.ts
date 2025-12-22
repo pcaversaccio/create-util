3cfc9f627002aff5cdae97b13342fa78f816121b
pcaversaccio <pascal.caversaccio@hotmail.ch>
2025-12-22 12:08:43 +0100

    🔁 Update Solidity to Version `0.8.33`
    Signed-off-by: pcaversaccio <pascal.caversaccio@hotmail.ch>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -66,7 +66,7 @@ const config: HardhatUserConfig = {
     // Only use Solidity default versions `>=0.8.20` for EVM networks that support the new `shanghai` opcode `PUSH0`:
     // https://github.com/ethereum/execution-specs/blob/forks/osaka/src/ethereum/forks/shanghai/__init__.py
     // Otherwise, use the versions `<=0.8.19`
-    version: "0.8.31",
+    version: "0.8.33",
     settings: {
       optimizer: {
         enabled: true,
